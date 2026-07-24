162f70583930dc673fc1626f7cb7a2671c9618c2
yyyyr1222 <rrr559574@gmail.com>
2026-07-24 13:52:37 +0700

    Add template part

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -20,36 +20,78 @@
 </template>
 
 <script setup>
-import { useQuasar } from 'quasar'
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-input color="purple-12" v-model="text" label="Label">
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-input color="teal" filled v-model="text" label="Label">
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-input
+        color="grey-3"
+        label-color="orange"
+        outlined
+        v-model="text"
+        label="Label"
+      >
+        <template v-slot:append>
+          <q-icon name="event" color="orange" />
+        </template>
+      </q-input>
+
+      <q-input
+        color="lime-11"
+        bg-color="green"
+        filled
+        v-model="text"
+        label="Label"
+      >
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+
+      <q-input color="teal" outlined v-model="text" label="Label">
+        <template v-slot:append>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg" />
+          </q-avatar>
+        </template>
+      </q-input>
+
+      <q-input
+        color="orange"
+        standout
+        bottom-slots
+        v-model="text"
+        label="Label"
+        counter
+        clearable
+      >
+        <template v-slot:prepend>
+          <q-icon name="place" />
+        </template>
+        <template v-slot:append>
+          <q-icon name="favorite" />
+        </template>
+
+        <template v-slot:hint> Field hint </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script setup>
 import { ref } from 'vue'
 
-const $q = useQuasar()
-
-const name = ref(null)
-const age = ref(null)
-const accept = ref(false)
-
-function onSubmit() {
-  if (accept.value !== true) {
-    $q.notify({
-      color: 'red-5',
-      textColor: 'white',
-      icon: 'warning',
-      message: 'You need to accept the license and terms first'
-    })
-  } else {
-    $q.notify({
-      color: 'green-4',
-      textColor: 'white',
-      icon: 'cloud_done',
-      message: 'Submitted'
-    })
-  }
-}
-
-function onReset() {
-  name.value = null
-  age.value = null
-  accept.value = false
-}
+const text = ref('')
+</script>
 </script>
